bcf8d35cf3ff3f50664af03f77d1439bb47fa907
Daniel Cooper <cooperdaniel5141@gmail.com>
2020-05-23 03:34:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,11 +5,14 @@ def key_for_min_value(name_hash)
 
 
 min = 1000000
+min_key = "a"
 name_hash.each do |key, value|
  if value < min
    min = value
+   min_key = key
  end
 end
+return key
 end
 
 
